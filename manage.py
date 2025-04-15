ee2a681e3fe1f4b289e45cafb7f8747e3af21839
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-15 18:20:31 +0200

    modified manage.py to set the local and production environment, also ran migrations

--- manage.py
+++ manage.py
@@ -3,10 +3,15 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    # Dynamically set the settings module based on the environment
+    environment = os.environ.get('DJANGO_ENV', 'local')
+    if environment == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.local')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +21,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
